07cd9a9a2b842b29eec635a4d4ed9fa3d5319d05
Alex <Alex@eduroam-220.epfl.ch>
2014-06-05 17:30:05 +0200

    Added Arduino code to control the motors

---
+++ servo12Stepper/servo12Stepper.ino
@@ -0,0 +1,95 @@
+#include <Servo.h>
+#include <AccelStepper.h>
+
+Servo myservo01,myservo02,myservo03,myservo04;
+int servoPin01 = 10;
+int servoPin02 = 9;
+int servoPin03 = 8;
+
+int directionPin01 = 2;
+int directionPin02 = 4;
+int stepPin01 = 3;
+int stepPin02 = 5;
+
+AccelStepper stepper01(1, stepPin01, directionPin01);
+AccelStepper stepper02(1, stepPin02, directionPin02);
+
+void setup() {
+  Serial.begin(19200);
+  myservo01.attach(servoPin01);
+  myservo02.attach(servoPin02);
+  myservo03.attach(servoPin03);
+  myservo01.write(92);
+  myservo02.write(97 + 35);
+  myservo03.write(65 + 24);
+  stepper01.setAcceleration(20000);
+  stepper02.setAcceleration(20000);
+}
+
+void loop() {
+  static int v = 0;
+
+  if (Serial.available()) {
+
+    char ch = Serial.read();
+    switch(ch){
+
+    case '0'...'9':
+      v = v * 10 + ch - '0';
+      break;  
+
+    case 'a': 
+      myservo01.write(v);
+      v = 0;
+      break;
+
+    case 'b':
+      myservo02.write(v);
+      v = 0;
+      break;   
+
+    case 'c':
+      myservo03.write(v);
+      v = 0;
+      break;
+
+    case 'd':
+      if(v < 12000){
+        stepper01.moveTo(-50000000); 
+        stepper01.setMaxSpeed(abs(v-12000));
+      }
+      else if (v >12000){
+        stepper01.moveTo(50000000);
+        stepper01.setMaxSpeed(abs(v-12000));
+      }
+      else {
+        stepper01.move(0);
+        stepper01.setMaxSpeed(0);
+      }
+      v = 0;
+      break;
+
+    case 'e':
+      if(v < 12000){
+        stepper02.moveTo(-50000000); 
+        stepper02.setMaxSpeed(abs(v-12000));
+      }
+      else if (v >12000){
+        stepper02.moveTo(50000000);
+        stepper02.setMaxSpeed(abs(v-12000));
+      }
+      else {
+        stepper02.move(0);
+        stepper02.setMaxSpeed(0);
+      }
+      v = 0;
+      break;
+    }  
+  }
+
+  Serial.flush();
+
+  stepper01.run();
+  stepper02.run();
+}
+
